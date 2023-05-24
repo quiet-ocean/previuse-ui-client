4cc83bdd8876ff2e6ed53c5ba0e749b602092b2e
GoodLuckMister <paet14@gmail.com>
2023-05-24 18:04:14 +0300

    changed to all campaigns

--- src/common/state/campaign/campaign.actions.ts
+++ src/common/state/campaign/campaign.actions.ts
@@ -1,14 +1,19 @@
-import createAsyncAction from '../../../utils/createAsyncAction';
-import { Campaigns } from '../../../swagger2Ts/interfaces';
-import httpService from '../../services/http.service';
-
+import createAsyncAction from "../../../utils/createAsyncAction";
+import { Campaigns } from "../../../swagger2Ts/interfaces";
+import httpService from "../../services/http.service";
 
 export enum CampaignActionTypes {
-  LIST_CAMPAIGNS = '@@campaign/LIST_CAMPAIGNS'
+  LIST_CAMPAIGNS = "@@campaign/LIST_CAMPAIGNS",
 }
 
-export const ListCampaignsAction: (clientId: number) => Promise<
-Campaigns[]
-> = createAsyncAction(CampaignActionTypes.LIST_CAMPAIGNS, (clientId: number) => {
-  return httpService.fetch({ url: `/campaigns/by_client/${clientId}` });
-});
+export const ListCampaignsAction: (clientId: number) => Promise<Campaigns[]> = createAsyncAction(
+  CampaignActionTypes.LIST_CAMPAIGNS,
+  (clientId: number) => {
+    return httpService.fetch({ url: `/campaigns` });
+  }
+);
+
+export const ListCampaignsByClientAction: (clientId: number) => Promise<Campaigns[]> =
+  createAsyncAction(CampaignActionTypes.LIST_CAMPAIGNS, (clientId: number) => {
+    return httpService.fetch({ url: `/campaigns/by_client/${clientId}` });
+  });
